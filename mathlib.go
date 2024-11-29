e8efc20da1c581d018746630b29a8f9f6f32fc90
Kyle Law <kylelaw1121@gmail.com>
2024-11-29 14:26:56 +0800

    Add Sqrt

--- mathlib.go
+++ mathlib.go
@@ -1,6 +1,9 @@
 package mathlib
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func Add(a, b int) int {
 	return a + b
@@ -19,4 +22,11 @@ func Divide(a, b int) (int, error) {
 		return 0, fmt.Errorf("cannot divide by zero")
 	}
 	return a / b, nil
+}
+
+func Sqrt(a float64) (float64, error) {
+	if a < 0 {
+		return 0, fmt.Errorf("cannot calculate square root of a negative number")
+	}
+	return math.Sqrt(a), nil
 }
